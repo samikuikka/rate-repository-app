a48cb7d1b0517b0ecc0943473c3dfc1f6184caf2
Sami Kuikka <sami.kuikka@aalto.fi>
2022-01-06 16:13:04 +0200

    10.11

---
+++ src/graphql/quesries.js
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`
+query {
+    repositories
+      {
+      edges {
+        node {
+          fullName,
+          id,
+          ratingAverage,
+          reviewCount,
+          stargazersCount,
+          watchersCount,
+          forksCount,
+          url,
+          ownerAvatarUrl,
+          description,
+          language
+        }
+      }
+    }
+  }
+`;
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,21 @@
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/quesries';
+import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+
+  const { data , error, loading } =  useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  });
+
+  useEffect( () => {
+    if(!loading) {
+      setRepositories(data.repositories)
+    }
+  },[loading])
+  
+  return { repositories, loading };
+};
+
+export default useRepositories;
